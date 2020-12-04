06ffe9cc4ae7bfb0027cfe10f908701c670a3a53
3atmancoding <3atmancoding@gmail.com>
2020-12-04 12:10:52 +0530

    Updated footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../homepage.css'
 const Footer = () =>{
-    return <footer className="footer">Made in India</footer>
+    return <footer className="footer">Designed and developed in India</footer>
 }
 export default Footer
