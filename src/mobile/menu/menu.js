176599e1812ac877a7883be6ce9c1d37e15be9e8
3atmancoding <3atmancoding@gmail.com>
2020-12-10 21:54:12 +0530

    Updated row-4

--- src/mobile/menu/menu.js
+++ src/mobile/menu/menu.js
@@ -1,30 +1,29 @@
-import React, {useState, useRef}from 'react'
-import Nav from '../../components/menu/nav'
-import './menu.css'
-import menuIcon from '../../assets/icons/menu2.svg'
-import closeIcon from '../../assets/icons/close.svg'
-const MobMenu = ()  =>{
-    const[isOpen, setOpen] = useState(false)
-    const Sidebar = ()=>{
-       return (
-       <>
-       <div className="sidebar">
-           <Nav/>
-       </div>
-       </>
-       )
-    }
-return(
+import React, { useState, useRef } from "react";
+import Nav from "../../components/menu/nav";
+import "./menu.css";
+import menuIcon from "../../assets/icons/menu2.svg";
+import closeIcon from "../../assets/icons/close.svg";
+const MobMenu = () => {
+  const [isOpen, setOpen] = useState(false);
+  const Sidebar = () => {
+    return (
+      <>
+        <div className="sidebar">
+          <Nav />
+        </div>
+      </>
+    );
+  };
+  return (
     <>
-    {
-        isOpen
-        ?
-        <Sidebar/>
-        :
-        ''
-    }
-    <img className = {isOpen ? "close-icon" : "mobile-icon"} src ={isOpen ? closeIcon: menuIcon} onClick = {()=>setOpen(!isOpen)}alt ="Menu icon"/>
-</>
-)
-}
+      {isOpen ? <Sidebar /> : ""}
+      <img
+        className={isOpen ? "close-icon" : "mobile-icon"}
+        src={isOpen ? closeIcon : menuIcon}
+        onClick={() => setOpen(!isOpen)}
+        alt="Menu icon"
+      />
+    </>
+  );
+};
 export default MobMenu;
