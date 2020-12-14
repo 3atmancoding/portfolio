921c7498f95e64023494f49f611e2590319e3d43
3atmancoding <3atmancoding@gmail.com>
2020-12-14 11:32:57 +0530

    Updated illustration and js file

--- src/components/summary/hello.js
+++ src/components/summary/hello.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './hello.css'
-import Image_1 from '../../assets/images/Me_v2.png'
+import Image_1 from '../../assets/images/Me_v3.png'
 const Hello = () =>{
     return <img src={Image_1} alt="This is a banner" className="imageProps"/>
 }
