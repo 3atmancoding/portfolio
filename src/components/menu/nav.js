50d383edeb435234ce4c908067dc89899d1e5b95
3atmancoding <3atmancoding@gmail.com>
2020-12-24 18:45:00 +0530

    Added no referrer to links

--- src/components/menu/nav.js
+++ src/components/menu/nav.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import './nav.css';
-import cvFile from '../../assets/files/CV-2020.pdf';
+import React from "react";
+import "./nav.css";
+import cvFile from "../../assets/files/CV-2020.pdf";
 const Nav = () => {
   return (
     <>
       <div className="navMenu">
-        <a href="https://utkillustrations.tumblr.com/" download>
+        <a
+          href="https://utkillustrations.tumblr.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Illustrations
         </a>
 
-        <a href="https://utkillustrator.tumblr.com/about">About</a>
+        <a
+          href="https://utkillustrator.tumblr.com/about"
+          target="_blank"
+          rel="noreferrer"
+        >
+          About
+        </a>
         <a href="mailto:hello@utkupadhyay.com">Hire me</a>
       </div>
     </>
